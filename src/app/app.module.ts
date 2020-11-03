fb71bec2b173a84ea89b694f35937e88255214d5
Eve <948832626@qq.com>
2020-11-03 18:52:49 +0800

    chore: add path config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { PlatformModule } from '@angular/cdk/platform';
 import { NgModule } from '@angular/core';
+import { NgxSignaturePadModule } from '@eve-sama/ngx-signature-pad';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-/**
- * You should import as below
- * import { NgxSignaturePadModule } from '@eve-sama/ngx-signature-pad';
- */
-import { NgxSignaturePadModule } from 'projects/ngx-signature-pad/src/public-api';
 import { DocumentComponent } from './document/document.component';
 import { MobileComponent } from './mobile/mobile.component';
 import { IconComponent } from './mobile/icon.component';
